493feb081f37b68a5da5bf5349c3357d935d328c
Chris Kielholz <Enorion@users.noreply.github.com>
2019-10-23 13:21:15 +0200

    Update Client_Packages

--- SirenSyncWithSilencer/client_packages/SirenSyncWithSilencer/index.js
+++ SirenSyncWithSilencer/client_packages/SirenSyncWithSilencer/index.js
@@ -7,15 +7,22 @@ mp.events.add('entityStreamIn', (entity) => {
     }
   }
   if (entity.type === 'vehicle') {
-    mp.events.callRemote("server:police:streamSiren", entity);
+    mp.events.callRemote("server:SirenSyncWithSilencer:streamSiren", entity);
   }
 });
 
-mp.events.add('client:police:streamSiren', (vehicle, sirenState) => {
+mp.events.add('client:SirenSyncWithSilencer:streamSiren', (vehicle, sirenState) => {
   vehicle.setSiren(sirenState);
 });
 
 mp.events.addDataHandler("silencer", (entity, value) => {
   if (entity.type === "vehicle")
     entity.setSirenSound(value);
+});
+
+mp.keys.bind(0x49, true, _ => {
+  if (player.vehicle && (player.vehicle.getPedInSeat(-1) === player.handle || player.vehicle.getPedInSeat(0) == player.handle) && player.vehicle.getClass() === 18) {
+    player.vehicle.getVariable('silencer') ? mp.game.graphics.notify(`~r~Silencer deactivated`) : mp.game.graphics.notify(`~g~Silencer activated`);
+    mp.events.callRemote('server:SirenSyncWithSilencer:syncSirens', player.vehicle)
+  }
 });
